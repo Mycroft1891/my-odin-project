232da8de3dae39477175f8174372a88b81bf90e6
Mycroft1891 <mycroft1891@gmail.com>
2018-01-06 22:48:58 +0100

    Add Caeser ruby project

---
+++ ruby_programming/caeser.rb
@@ -0,0 +1,17 @@
+class String
+
+  def shift_char(n)
+    return (self.ord - 97 + n) % 26 + 97 if (('a'..'z').include?(self))
+    return (self.ord - 65 + n) % 26 + 65 if (('A'..'Z').include?(self))
+    self
+  end
+
+end
+
+def caeser(string, n)
+  return string.split('').map {|c| c.shift_char(n) }.map(&:chr).join('')
+end
+
+p caeser('hello', 1)        # ifmmp
+p caeser('world', 1)        # xpsme
+p caeser('hello world', 1)  # ifmmp xpsme
