c4ba8524d7a7c5fad5f5101c2d85e0aff9adae9f
Mycroft1891 <mycroft1891@gmail.com>
2018-01-12 18:10:54 +0100

    Finish up mastermind program

--- ruby_programming/mastermind/Little_Bot.rb
+++ ruby_programming/mastermind/Little_Bot.rb
@@ -4,36 +4,37 @@ class Little_Bot
     @color_options = ['R', 'G', 'B', 'P']
     @guesses = [@color_options[0..3]]
     @hints = []
-    @responses = [
-                    "This might work\n",
-                    "I think this is right\n",
-                    "I hope I guess it right\n",
-                    "Maybe this ?\n"
-                  ]
+    @responses = ["This might work\n",
+                  "I think this is right\n",
+                  "I hope I guess it right\n",
+                  "Maybe this ?\n"]
+    @code_length = 4
   end
 
   def get_input
     puts "#{@responses[Random.rand(@responses.size)]}"
-    return @guesses[0].join if @guesses.size < 2
-
-    @guesses.last
+    return @guesses.first.join if @guesses.size < 2
+    @guesses.last.join
   end
 
   def store_board_response(response)
-    [["R", "G", "B", "P"], ["0", "0", "0", "1"]]
-    puts "#{response}"
     @hints << response.last
 
     new_guess = []
-    for i in (0..@guesses[0].size) # plug the right guesses
-      new_guess[i] = @guesses.last[i] if @hints.last[i] == "1"
+    keep = []
+    for i in (0..@code_length) # plug the right guesses
+      new_guess[i]    = @guesses.last[i] if @hints.last[i] == "1"
+      keep << @guesses.last[i] if @hints.last[i] == "0"
+    end
+
+    keep.shuffle!
+
+    for i in (0..@code_length)
+      new_guess[i] = keep.first if @hints.last[i] == "0"
+      keep.shift
     end
 
     @guesses << new_guess
-  end
-
-  def get_guess_for(num)
-
   end
 
 end
--- ruby_programming/mastermind/main.rb
+++ ruby_programming/mastermind/main.rb
@@ -1,6 +1,20 @@
 require './Board'
 require './Little_Bot'
 
+def display_rules
+  puts "===== Rules ====="
+  puts "The codebreaker has 10 attempts to guess the right colors and their possitons."
+  puts "These are the colors the codebreaker can choose from:\n"
+  puts "\tRed (R)\n\tGreen (G)\n\tBlue (B)\n\tPurple (P)\n\n"
+  puts "The codebreaker can submit his/her guess in lower case:\n"
+  puts "ex:\trgbp\n\n"
+  puts "I will give the codebreaker hints after each try\n"
+  puts "ex:\t[0,1,0,0]\n\n"
+  puts "0: for a right color at a wrong place\n"
+  puts "1: for a right color at the right place\n"
+  puts "So lets start guessing: ...\n"
+end
+
 def start_codemaker_mode
   puts "You chose to be the Codemaker, you can chose from these colors:\n"
   puts "\tRed (R)\n\tGreen (G)\n\tBlue (B)\n\tPurple (P)\n\n"
@@ -13,11 +27,9 @@ def start_codemaker_mode
   while board.player_guessed_right != true
     puts "\nGuess my Code:\n"
     bot_input = bot.get_input
-    sleep 2
-    puts bot_input
-    sleep 2
+    sleep 1
     puts board.check(bot_input)
-    sleep 2
+    sleep 1
     bot.store_board_response(board.response)
   end
 end
@@ -33,24 +45,7 @@ def start_codebreaker_mode
   end
 end
 
-def display_rules
-  puts "===== Rules ====="
-  puts "The codebreaker has 10 attempts to guess the right colors and their possitons."
-  puts "These are the colors the codebreaker can choose from:\n"
-  puts "\tRed (R)\n\tGreen (G)\n\tBlue (B)\n\tPurple (P)\n\n"
-  puts "The codebreaker can submit his/her guess in lower case:\n"
-  puts "ex:\trgbp\n\n"
-  puts "I will give the codebreaker hints after each try\n"
-  puts "ex:\t[0,1,0,0]\n\n"
-  puts "0: for a right color at a wrong place\n"
-  puts "1: for a right color at the right place\n"
-  puts "So lets start guessing: ...\n"
-end
-
-
 puts "Welcome to Master Minds\n"
-puts "Chose your play mode by typing choice:\n"
-puts "\tCodemaker"
-puts "\tCodebreaker"
+puts "Chose your play mode by typing your choice:\n\tCodemaker\n\tCodebreaker"
 user_choice = gets.chomp
 /maker/.match(user_choice) ? start_codemaker_mode : start_codebreaker_mode
